ea66ab03e0badb60648c5927d48663b436aa7de1
Vitor <vitoraugusto_14@outlook.com>
2023-09-18 16:29:54 -0300

    Project setup

---
+++ scripts/Test.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class Test : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GD.Print("Hello Godot! I hope we become good friends :)");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+	}
+}
